5f1d64b2f32da938dad7f0ca17bf0aac317ed9a0
Silvano Coriani <scoriani@microsoft.com>
2019-04-01 17:41:17 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reliable_dal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
